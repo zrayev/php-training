65de007b369be896f4c381531f6692478c1e991e
LizabethEpifanova <ladydowaakhin@gmail.com>
2016-04-14 22:12:56 +0300

    Add

--- index.php
+++ index.php
@@ -3,8 +3,11 @@ require_once('results.php');
 
 $dice_type = isset($_GET['d']) ? $_GET['d'] : 6;
 
-$count = isset($_GET['count']) ? $_GET['count'] : 1;
+$count = isset($_GET['count']) ? $_GET['count'] : 7;
+
+$min = isset($_GET['min']) ? $_GET['min'] : 3;
 
 for ($i = 0; $i < $count; $i++) {
-  echo get_single_dice_result($dice_type) . "<br/>";
+  
+  echo get_single_dice_result($dice_type,$min) . "<br/>";
 }
--- results.php
+++ results.php
@@ -1,10 +1,5 @@
 <?php
-function get_dice_results($dice_type, $count) {
-  for ($i = 0; $i < $count; $i++) {
-    echo "<p> dice " . ($i+1) . " result is "  . rand(1, $dice_type) . "</p><br/>";
-  }
-}
-
-function get_single_dice_result($dice_type) {
-  return rand(1, $dice_type);
+function get_single_dice_result($dice_type,$min) {
+  $result=rand(1,$dice_type);
+  return ($result>=$min) ? $result : ' ' ;
 }
