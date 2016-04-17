20556af2b64c2a6b14f74e8ca092b52f63ec655f
feshchenkostanislav@gmail.com <Atotyrj2580>
2016-04-17 20:26:34 +0300

    Game Dice Show

--- index.php
+++ index.php
@@ -1,10 +1,31 @@
-<?php
-require_once('results.php');
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Game</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+  <body>
 
-$dice_type = isset($_GET['d']) ? $_GET['d'] : 6;
+  <?php require_once('results.php'); ?>
 
-$count = isset($_GET['count']) ? $_GET['count'] : 1;
+    <form action="" method="get">
+      <p>Выберите количество граней</p>
+      <input type="number" name="facet" value="1" min="1" max="6">
+      <p>Выберите количество бросков кубика</p>
+      <input type="number" name="throw" value="1" min="1" max="20">
+      <p>Меньше какого значения не выводить</p>
+      <input type="number" name="min_value" value="1" min="1" max="6">
+      <p>Поставте точку если хотине не выводить результат маньше 3х</p>
+      <input type="radio" name="restriction" value="3">Значение меньше 3х</br></br>
+      <input type="submit" value="Submit">
+    </form>
 
-for ($i = 0; $i < $count; $i++) {
-  echo get_single_dice_result($dice_type) . "<br/>";
-}
+    <?php for ($i = 0; $i < $count; $i++): ?>
+    <!--Меняем значене класа в зависимоти от возвращаемого значения-->
+    <div class="dice-<?php echo get_single_dice_result($min_value, $dice_type, $restriction); ?> dices"></div><br/>
+    <?php endfor; ?>
+
+  </body>
+
+</html>
--- results.php
+++ results.php
@@ -1,10 +1,21 @@
 <?php
-function get_dice_results($dice_type, $count) {
-  for ($i = 0; $i < $count; $i++) {
-    echo "<p> dice " . ($i+1) . " result is "  . rand(1, $dice_type) . "</p><br/>";
+
+  $dice_type = isset($_GET['facet']) ? $_GET['facet'] : 6;//количество граней куба
+  $min_value = isset($_GET['min_value']) ? $_GET['min_value'] : 1;//Минимальное значение для вывода
+  $count = isset($_GET['throw']) ? $_GET['throw'] : 0;//Количество бросков
+  $restriction = isset($_GET['restriction']) ? $_GET['restriction'] : false;//Тригер
+
+function get_single_dice_result( $min_value, $dice_type, $restriction) {
+  if (!$restriction){//стандартная логика
+    return rand($min_value, $dice_type);
+  }else {//Подключение логики с репозитория https://github.com/ElderPhoenix/php-training/blob/logic/php-training/results.php
+    $number = rand(1, $dice_type);
+    if ($number > 3){
+      return $number;
+    } else {
+      return $message = "7";//Возращаемое значения если тригер активен
+    }
   }
 }
 
-function get_single_dice_result($dice_type) {
-  return rand(1, $dice_type);
-}
+?>
