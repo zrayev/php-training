67cee46c9a4a7375bbf2b3263e1587d1da978e8b
adaMesser <ladydowaakhin@gmail.com>
2016-04-21 01:31:19 +0300

    dice game with show order and tho forms

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $options = array(
   'dice_type' => isset($_POST['facet']) ? $_POST['facet'] : 6,
   'min_value' => isset($_POST['min_value']) ? $_POST['min_value'] : 1,
   'count' => isset($_POST['throw']) ? $_POST['throw']: 0,
+  'trigger' => isset($_POST['trigger']) ? $_POST['trigger'] : 0,
 );
 
 include_once('main.php');
--- main.php
+++ main.php
@@ -9,40 +9,36 @@
 <body>
 <div class="center">
   <div class="content">
-    <form action="" method="post" role="form">
+    <form action="show_results.php" method="post" role="form">
       <div class="form-group">
-        <label>
-          <p>Выберите количество граней</p>
-          <input type="number" name="facet" value="1" min="1" max="6"
-                 class="form-control">
-        </label>
+        <label for="inp-facet">Выберите количество граней</label>
+        <input id="inp-facet" type="number" name="facet" value="1" min="1" max="6" class="form-control">
       </div>
       <div class="form-group">
-        <label>
-          <p>Выберите количество бросков кубика</p>
-          <input type="number" name="throw" value="1" min="1" max="20"
-                 class="form-control">
-        </label>
+        <label for="inp-throw">Выберите количество бросков кубика</label>
+        <input id="inp-throw" type="number" name="throw" value="1" min="1" max="20" class="form-control">
       </div>
+
       <div class="form-group">
-        <label>
-          <p>Меньше какого значения не выводить</p>
-          <input type="number" name="min_value" value="1" min="1" max="6"
-                 class="form-control">
-        </label>
+        <label for="inp-minvalue">Меньше какого значения не выводить</label>
+        <input id="inp-minvalue" type="number" name="min_value" value="1" min="1" max="6" class="form-control">
       </div>
+
+      <div class="form-group">
+        <label for="sort_order">Выберите порядок сортировки результатов</label>
+          <select id="sort_order" size="1" name="trigger">
+						<option value="0">Не сортировать</option>
+            <option value="1">По возрастанию</option>
+            <option value="2">По убыванию</option>
+          </select>
+      </div>
+
+
       <div class="btn_center">
         <input type="submit" value="Играть" class="btn btn-danger">
       </div>
     </form>
 
-    <?php $results = get_dice_results($options['dice_type'], $options['count']); ?>
-    <?php foreach ($results as $result): ?>
-      <!--Меняем значене класа в зависимоти от возвращаемого значения-->
-      <?php if ($result >= $options['min_value']): ?>
-        <div class="dice-<?php echo $result; ?> dices"></div><br/>
-      <?php endif; ?>
-    <?php endforeach; ?>
   </div>
 </div>
 </body>
