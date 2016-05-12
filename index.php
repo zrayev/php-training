3f3aca83f980067564df6f9f2f7e761ce70e3e8c
dmitry <zrayev@mail.ru>
2016-05-12 13:37:07 +0300

    refactor code

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
+
 //calculation functions
 require_once('results.php');
 if (!empty($_POST)) {
-    $command_parser = $_POST['command_parser'];
-    include_once('show_results.php');
+  $command_parser = $_POST['command_parser'];
+  include_once('show_results.php');
 } else {
-    include_once('main.php');
+  include_once('main.php');
 }
--- main.php
+++ main.php
@@ -1,24 +1,24 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Game</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <meta charset="UTF-8">
+  <title>Game</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 </head>
 <body>
 <div class="center">
-    <div class="content">
-        <form action="" method="post" role="form">
-            <div class="form-group">
-                <label for="inp-facet">Команда:</label>
-                <input id="inp-facet" type="text" name="command_parser" size="30" value="throw 10 d6 filter >= 2 sort desc" class="form-control">
-            </div>
-            <div class="btn_center">
-                <input type="submit" value="Грати" class="btn btn-danger">
-            </div>
-        </form>
-    </div>
+  <div class="content">
+    <form action="" method="post" role="form">
+      <div class="form-group">
+        <label for="inp-facet">Команда:</label>
+        <input id="inp-facet" type="text" name="command_parser" size="30" value="throw 10 d6 filter >= 2 sort desc" class="form-control">
+      </div>
+      <div class="btn_center">
+        <input type="submit" value="Грати" class="btn btn-danger">
+      </div>
+    </form>
+  </div>
 </div>
 </body>
 </html>
